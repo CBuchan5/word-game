fee4b3c535b586570a57b00e5f26c8d685a831b1
dhopz <david_houpapa@hotmail.com>
2022-07-22 15:53:56 +0100

    working on CLI

--- app/src/main/java/game/App.java
+++ app/src/main/java/game/App.java
@@ -1,4 +1,5 @@
 package game;
+import java.util.*;
 
 public class App {
     public String getGreeting() {
@@ -11,5 +12,9 @@ public class App {
         Game game = new Game(wordChooser);
         System.out.println(game.getWordToGuess());
         System.out.println("Enter one letter to guess (10 attempts remaining):");
+        Scanner sc= new Scanner(System.in);
+        String a= sc.nextLine(); 
+        System.out.print(a);
+        sc.close();
     }
 }
